df365ee1fd14a34e7dea90e47d8bb77aa44cb47d
Kryptanyte <ryanfournier2@hotmail.com>
2018-02-15 19:17:58 +1300

    Virtural

---
+++ Class Transcripts/VMDvdDrive.ps1
@@ -0,0 +1,8 @@
+#Setting DVD drive through Powershell
+
+Set-VMDvdDrive -VMName "(Virtual Machine Name)" -Path "(Enter .Iso Path Here)"
+
+#Set-VMDvdDrive - Allows you to set a DVD drive on a virtual Machine
+
+#Make sure to use ./image-ddwrt.sh if this is the DD-WRT Router
+
