3e592b1b8273318704c1c05b5471733ee935a6ee
Kryptanyte <ryanfournier2@hotmail.com>
2018-02-28 19:18:57 +1300

    Fixes + Additions

--- VM Creation/vm.ps1
+++ VM Creation/vm.ps1
@@ -7,12 +7,15 @@ Param(
 	[string]$ISO,
 	[ValidateRange(1,2)]
 	[Int16]$Generation,
-	[ValidatePattern("^[0-9]*(?:T|GB|MB)$")]
+	[ValidatePattern("^[0-9]*(?:TB|GB|MB)$")]
 	[string]$MinMem,
-	[ValidatePattern("^[0-9]*(?:T|GB|MB)$")]
+	[ValidatePattern("^[0-9]*(?:TB|GB|MB)$")]
 	[string]$StartMem,
-	[ValidatePattern("^[0-9]*(?:T|GB|MB)$")]
-	[string]$MinDiskSize
+	[ValidatePattern("^[0-9]*(?:TB|GB|MB)$")]
+	[string]$DiskSize,
+	
+	[switch]$Terminal,
+	[switch]$Desktop
 )
 
 ########################
@@ -23,6 +26,8 @@ Param(
 $ConfigFileLoc = (Split-Path $MyInvocation.MyCommand.Path -Parent) + "\conf"
 $JsonPath = $ConfigFileLoc + "\OperatingSystems.json"
 $OSList = ((Get-Content $JsonPath) -join "`n" | ConvertFrom-Json).OperatingSystems
+
+$OSConf
 
 ###########################
 ##	Re-usable Functions  ##
@@ -39,24 +44,46 @@ function InBytes
 	return [scriptblock]::Create($Size).InvokeReturnAsIs()
 }
 
-function IsValidVar
+function IsValidPath
 {
 	Param(
 		[Parameter(Mandatory = $True)]
-		$Var
+		[string]$Path,
+		
+		[Parameter(Mandatory = $False)]
+		[string]$PathType = "Leaf"
 	)
 	
-	return switch -Wildcard ($Var.GetType().Name) {
-		"String"
-		{
-			[string]::IsNullOrEmpty($Var)
-		}
-	}
+	return Test-Path -Path $Path -PathType $PathType
 }
 
 #############################
 ##	Main Script Functions  ##
 #############################
+
+function PARAMVALIDATION
+{
+	if($ISO -and (-not (IsValidPath -Path $ISO)))
+	{
+		Write-Error -Message "Invalid ISO Path" -Category ReadError -CategoryActivity "Parameter Validation" -CategoryReason "File Path Invalid" -CategoryTargetName $ISO
+		exit 1
+	}
+	
+	if($MinMem -and ($MinMem -lt $OSConf.Settings.MinMem))
+	{
+		#Error
+		exit 1
+	}
+	
+	if($MinMem -and $MaxMem)
+	{
+		if($MinMem -gt $MaxMem)
+		{
+			#Error
+			exit 1
+		}
+	}
+}
 
 # Check for Valid Edition in Config
 function GetEdition
@@ -73,44 +100,56 @@ function GetEdition
 	
 	# No Valid Name Was Found, Exit with Error
 	Write-Error -Message "Invalid Edition Name" -Category ObjectNotFound -CategoryActivity "CheckEdition" -CategoryReason "Edition Not Found in OS List." -CategoryTargetName $Edition
+	exit 1
 }
 
 function GetVMName
 {
-	Parameter(
+	Param(
 		[Parameter(Mandatory = $True)]
 		[string]$VMName
 	)
 	
-	return (Get-VM -Name ($VMName + "*") | measure).count + 1
+	return $VMName + ([int]((Get-VM -Name ($VMName + "*") | measure).count) + 1)
 }
 
 # Verify Passed Variables with Config
 function VerifySettings
 {
-	Param(
-		[Parameter(Mandatory = $True)]
-		$OSConf
-	)
-	
 	$VMConf = @{
 		"New-VM"=@{}
+		"Set-VMDvdDrive"=@{}
 	}
-	if(IsValidVar($ISO))
+	
+	$VMConf["New-VM"]["Name"] = $OSConf.Name
+	$VMConf["New-VM"]["MemoryStartupBytes"] = if($StartMem){$StartMem}else{$OSConf.Settings.StartMem}
+	$VMConf["New-VM"]["NewVHDPath"] = ($OSConf.Name + "_System.vhdx")
+	$VMConf["New-VM"]["NewVHDSize"] = if($DiskSize){$DiskSize}else{$OSConf.Settings.MinDiskSize}
+	$VMConf["New-VM"]["Generation"] = $OSConf.Settings.Generation
+	
+	if($OSConf.Settings.InstallType -eq "Server")
 	{
-		
+		if($Desktop)
+		{
+			write-host "test"
+		}
 	}
-	else
-	{
-		$VMConf[
-	}
+	#$VMConf["Set-VM"]["MemoryMinimumBytes"] = 
+	
+	$VMConf["Set-VMDvdDrive"]["Path"] = if($ISO){$ISO}else{$OSConf.Settings.ISO}
+	
+	return $VMConf
 }
 
 function Main
 {
+	# Work around for Powershell Validation Problems, DO NOT TOUCH
+	PARAMVALIDATION 
+	# Below here can be touched
+
 	$OSConf = GetEdition
 	
-	$VMConf = VerifySettings($OSConf)
+	$VMConf = VerifySettings
 }
 
 Main
