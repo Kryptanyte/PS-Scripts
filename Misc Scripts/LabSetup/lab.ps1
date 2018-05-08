2cc909b6c9c0b421c8295d65b9bfb86de4e22a0b
Kryptanyte <ryanfournier2@hotmail.com>
2018-05-08 13:06:28 +1200

    New Creationg Script Updates

--- Misc Scripts/LabSetup/lab.ps1
+++ Misc Scripts/LabSetup/lab.ps1
@@ -1,52 +1,33 @@
+$reg = "^(?:[a-z]+) (.+)=(.*)$"
+
 function GetServerConfigs
 {
   $Servers = [System.Collections.ArrayList]@()
   Get-ChildItem "$PSScriptRoot/VMs" -Filter *.json |
   ForEach-Object {
-    $Servers.Add((Get-Content $_.FullName | ConvertFrom-Json)) > $null
+    $t = ((Get-Content $_.FullName) | ConvertFrom-Json).PSObject.Properties
+
+    ForEach($v in $t)
+    {
+      Write-Host $v.Name
+    }
+
   }
 
 	return $Servers
 }
 
-Clear-Host
-
-
-
-$ServerNames = @(
-  "LON-DC1",
-  "LON-SVR1",
-  "LON-CL1",
-  "EU-RTR",
-  "TOR-SVR1",
-  "INET"
-)
+$Servers = GetServerConfigs
 $ToInstall = new-object bool[] $ServerNames.Length
-<#
-Write-Host -Object @"
 
-    1) LON-DC1
-    2) LON-SVR1
-    3) LON-CL1
-    4) EU-RTR
-    5) TOR-SVR1
-    6) INET
-
-    a) Accept
-    q) Quit Setup
-
-"@
-
-$result = Read-Host "Select an Option"
-#>
 Write-Host -ForegroundColor Cyan -Object @"
 
       Lab VM List
 
 "@
 
-$i = 0
-foreach($name in $ServerNames)
+
+ForEach($Serv in $Servers)
 {
-  Write-Host "$name"
+  #Write-Host $Serv[0]
 }
