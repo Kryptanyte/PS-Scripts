61f1df087ed70c910876804354cfcd1d12043c86
Kryptanyte <ryanfournier2@hotmail.com>
2018-05-09 20:57:30 +1200

    Ascii Converter working

--- Misc Scripts/ascii.ps1
+++ Misc Scripts/ascii.ps1
@@ -5,8 +5,7 @@ Param(
 	[Parameter(Mandatory = $False)]
 	[string]$EncryptionKey,
 	[int]$EncryptionMulti,
-	[switch]$Sum = $False,
-	[switch]$InlineCodes = $False,
+	[switch]$SumString = $False,
 	[switch]$Decode = $False
 )
 
@@ -20,7 +19,7 @@ function StrToCodes
 	$CharArr = $ToConvert.ToCharArray()
 	$ReturnArr = [System.Collections.ArrayList]@()
 
-	ForEach($char in $CharArr)
+	Foreach($char in $CharArr)
 	{
 		$ReturnArr.Add(([int][char]$char)) > $null
 	}
@@ -35,9 +34,32 @@ function SumArr
 		$Arr
 	)
 
-	$Sum
-	$Arr | Foreach {$Sum += $_}
+	$Sum = 0
+
+	$Arr | Foreach { $Sum += $_ }
+
 	return $Sum
+}
+
+function GetMulti
+{
+	$Multiplier = 1
+
+	if($EncryptionMulti -eq 0)
+	{
+		if($EncryptionKey -ne "")
+		{
+			$Multiplier = SumArr((StrToCodes($EncryptionKey)))
+
+			$Enc = $True
+		}
+	}
+	elseif($EncryptionMulti -gt 1)
+	{
+		$Multiplier = $EncryptionMulti
+	}
+
+	return $Multiplier
 }
 
 function Encode
@@ -48,132 +70,60 @@ function Encode
 	)
 
 	$EncodedArr = StrToCodes($RawString)
-	$Enc = $False
-	$Multi
+	$Multi = GetMulti
 
-	if($EncryptionMulti -eq 0)
-	{
-		if($EncryptionKey -ne "")
-		{
-			$Multi = SumArr((StrToCodes($EncryptionKey)))
+	$EncryptedArr = [System.Collections.ArrayList]@()
 
-			$Enc = $True
-		}
-	}
-	elseif($Multi -gt 0)
+	Foreach($Value in $EncodedArr)
 	{
-		$Enc = $True
+		$EncryptedArr.Add(($Value * $Multi)) > $null
 	}
 
-	if($Enc)
-	{
-		Foreach($Value in $EncodedArr)
-		{
-			$Value = $Value * $Multi
-		}
-	}
-
-	return ($EncodedArr -join " ")
-}
-
-Encode($InputString)
-
-$EncryptionMulti
-
-<#
-
-if($Key -ne "")
-{
-	$CharArr = $Key.ToCharArray()
-	$Total
-	ForEach($char in $CharArr)
-	{
-		$code = [int][char]$char
-
-		$Total += $code
-	}
-
-	$Multiplier = $Total
+	return ($EncryptedArr -join " ")
 }
 
 function Decode
 {
-	$CodeArr = $ToConvert.Split(" ")
-	$StrArr = [System.Collections.ArrayList]@()
+	Param(
+		[Parameter(Mandatory = $True)]
+		[string]$RawString
+	)
 
-	ForEach($code in $CodeArr)
+	$EncodedArr = $RawString.Split(" ")
+	$Multi = GetMulti
+
+	$StringArr = [System.Collections.ArrayList]@()
+
+	Foreach($Value in $EncodedArr)
 	{
-		if($Multiplier -gt 0)
-		{
-			$code = ($code / $Multiplier)
-		}
-
-		$char = [char][int]$code
-
-		$StrArr.Add($char) > $null
+		$StringArr.Add(([char][int]($Value / $Multi))) > $null
 	}
 
-	Write-Host -ForegroundColor Yellow ($StrArr -Join "")
-
+	return ($StringArr -join "")
 }
 
-function Encode
+function Main
 {
-	$CharArr = $ToConvert.ToCharArray()
-	$CodeArr = [System.Collections.ArrayList]@()
-	$CodeStr = ""
-	$Total = 0
-
-	foreach($char in $CharArr)
+	if($Decode)
 	{
-		$code = [int][char]$char
+		$DecodedStr = Decode($InputString)
 
-		if($Multiplier -gt 0)
-		{
-			$code = $code * $Multiplier
-		}
-
-		if($SumChar)
-		{
-			$Total += $code
-		}
-
-		$CodeArr.Add($code) > $null
-
-		if($CodeStr -eq "")
-		{
-			$CodeStr = [string]$code
-		}
-		else
-		{
-			$CodeStr = $CodeStr + " " + [string]$code
-		}
+		Write-Host $DecodedStr
 	}
-
-	if($InlineCodes)
+	elseif($SumString)
 	{
-		Write-Host -ForegroundColor Yellow $ToConvert
-		Write-Host -ForegroundColor Green $CodeStr
-	}
-	elseif($SumChar)
-	{
-		Write-Host -ForegroundColor Yellow $Total
+		$Multi = GetMulti
+		$CodeArr = [System.Collections.ArrayList]@()
+	  (StrToCodes($InputString)) | Foreach {$CodeArr.Add(($_ * $Multi))} > $null
+		Write-Host (SumArr($CodeArr))
 	}
 	else
 	{
-		For($i = 0; $i -lt $CharArr.Length; $i++)
-		{
-			Write-Color -Text ($CharArr[$i]), " | ", ($CodeArr[$i]) -Color Yellow,White,Green
-		}
+		$EncodedStr = Encode($InputString)
+
+		Write-Host $InputString
+		Write-Host $EncodedStr
 	}
 }
 
-if($Decode)
-{
-	Decode
-}
-else
-{
-	Encode
-}
-#>
+Main
