b3564017a2c7e2830460fe8cb9d472ac16ce2dae
Kryptanyte <ryanfournier2@hotmail.com>
2018-05-14 11:55:07 +1200

    Expanding Classes

--- Misc Scripts/LabSetup/t.ps1
+++ Misc Scripts/LabSetup/t.ps1
@@ -6,30 +6,105 @@ class VHD
   [UInt64]$Size
   [String]$Parent
   [Switch]$Dynamic
+
+  [Switch] VerifyVHD()
+  {
+    if($this.Name -eq "")
+    {
+      return $False
+    }
+
+    if($this.Type -eq "")
+    {
+      return $False
+    }
+    else
+    {
+      if(-not ("Fixed Size","Dynamically Expanding","Differencing").Contains($this.Type))
+      {
+        return $False
+      }
+      else
+      {
+        if($this.Type -eq "Differencing")
+        {
+          if($this.Type -eq "")
+          {
+            return $False
+          }
+          else
+          {
+            <#
+            if(-not (Test-Path -Path $this.Name -PathType Leaf))
+            {
+              return $False
+            }
+            #>
+          }
+        }
+      }
+    }
+  }
 }
 
 class Server
 {
   [String]$Name
   [VHD[]]$VHDs
+  [Int32]$Generation
+  [UInt64]$MinMemory
+  [UInt64]$MaxMemory
+  [String]$InstallISO
+  [String]$Network
+
+  [Switch] VerifyServer()
+  {
+    if(($this.Generation -lt 1) -or ($this.Generation -gt 2))
+    {
+      return $False
+    }
+
+    if(-not (Get-VMSwitch -Name $this.Switch -ErrorAction SilentlyContinue))
+    {
+      return $False
+    }
+
+    Foreach($VHD in $this.VHDs)
+    {
+      if(-not $VHD.VerifyVHD())
+      {
+        return $False
+      }
+    }
+    return $True
+  }
+
+  [void] Create()
+  {
+    if(-not (Get-VMSwitch -Name $this.Switch -ErrorAction SilentlyContinue))
+    {
+      return
+    }
+
+    $VM = New-VM -Name $this.Name -Generation $this.Generation
+  }
 }
 
-$vhd1 = [VHD]@{
+$svr = [Server]::new()
+
+$svr.VHDs += [VHD]@{
   Name = "Test.vhdx";
   Type = "Differencing";
   Parent = "parent.vhdx";
 }
 
-$vhd2 = [VHD]@{
+$svr.VHDs += [VHD]@{
   Name = "Test2.vhdx";
   Type = "Differencing";
   Parent = "parent.vhdx";
 }
 
-$svr = [Server]::new()
-
-$svr.VHDs += $vhd1
-
-$svr.VHDs += $vhd2
-
-$svr
+Foreach($VHD in $svr.VHDs)
+{
+  $VHD.Name
+}
